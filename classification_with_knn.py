ec77e259b2b02c087db7d2f8e41c1a654e367262
jhoarau <jeanemmanuel.hoarau@etu.enseeiht.fr>
2021-12-03 17:27:06 +0100

    update

--- classification_with_knn.py
+++ classification_with_knn.py
@@ -18,31 +18,33 @@ ver_1 = np.load("C:/Users/jemho/Documents/Etude/hambourg/cours_hambourg/intellig
 ver_2 = np.load("C:/Users/jemho/Documents/Etude/hambourg/cours_hambourg/intelligent system in medicin/pbl/Feature Vectors/X_ver_gray_entropy.npy")
 ver_3 = np.load("C:/Users/jemho/Documents/Etude/hambourg/cours_hambourg/intelligent system in medicin/pbl/Feature Vectors/X_ver_gray_rms.npy")
 ver_4 = np.load("C:/Users/jemho/Documents/Etude/hambourg/cours_hambourg/intelligent system in medicin/pbl/Feature Vectors/X_ver_gray_kurtosis.npy")
-ver_5 = np.load("C:/Users/jemho/Documents/Etude/hambourg/cours_hambourg/intelligent system in medicin/pbl/Feature Vectors/X_ver_gray_std.npy")
-ver_6 = np.load("C:/Users/jemho/Documents/Etude/hambourg/cours_hambourg/intelligent system in medicin/pbl/Feature Vectors/X_ver_gray_skewness.npy")
+ver_5 = np.load("C:/Users/jemho/Documents/Etude/hambourg/cours_hambourg/intelligent system in medicin/pbl/Feature Vectors/X_ver_gray_skewness.npy")
+ver_6 = np.load("C:/Users/jemho/Documents/Etude/hambourg/cours_hambourg/intelligent system in medicin/pbl/Feature Vectors/X_ver_gray_std.npy")
 
 y_ver = np.load("C:/Users/jemho/Documents/Etude/hambourg/cours_hambourg/intelligent system in medicin/pbl/Feature Vectors/y_ver.npy")
 
 
 # création de la matrice qui entraïne le model
 
-features = np.array([features_1,features_2,features_3,features_4,features_5,features_6])
+# features = np.array([features_1,features_2,features_3,features_4,features_5,features_6])
+features = np.array([features_2,features_3,features_5,features_6])
 features = np.transpose(features)
 
 # création de la matrice pur testé le model
 
-ver = np.array([ver_1,ver_2,ver_3,ver_4,ver_5,ver_6])
+# ver = np.array([ver_1,ver_2,ver_3,ver_4,ver_5,ver_6])
+ver = np.array([ver_2,ver_3,ver_5,ver_6])
 ver = np.transpose(ver)
 
 # création et entrainement du model
 
-knn_model = KNeighborsClassifier(n_neighbors=20)
-knn_model.fit(features,labels)
-
-# prediction du model
-predicted = knn_model.predict(ver)
-acc = accuracy_score(y_ver,predicted)
-print(acc)
+# knn_model = KNeighborsClassifier(n_neighbors=20)
+# knn_model.fit(features,labels)
+#
+# # prediction du model
+# predicted = knn_model.predict(ver)
+# acc = accuracy_score(y_ver,predicted)
+# print(acc)
 
 
 # ce que fait le accuracy_score
@@ -57,15 +59,20 @@ print(acc)
 
 # cherccher le meilleur nombre de voisin
 
-# L1=[]
-# L2=[]
-# for k in range(1,500,5):
-#     knn_model = KNeighborsClassifier(n_neighbors=k)
-#     knn_model.fit(features,labels)
-#     predicted = knn_model.predict(ver)
-#     acc = acce(y_ver,predicted)
-#     L1.append(k)
-#     L2.append(acc)
-#
-# plt.plot(L1,L2)
-# plt.show()
+L1=[]
+L2=[]
+for k in range(1,500,5):
+    knn_model = KNeighborsClassifier(n_neighbors=k)
+    knn_model.fit(features,labels)
+    predicted = knn_model.predict(ver)
+    acc = accuracy_score(y_ver,predicted)
+    L1.append(k)
+    L2.append(acc)
+max_value = max(L2)
+max_index = L2.index(max_value)
+best_n_neighbour = L1[max_index]
+
+print(max_value)
+print("best_neighbour is ",best_n_neighbour)
+plt.plot(L1,L2)
+plt.show()
