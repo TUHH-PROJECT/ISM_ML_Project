302e9a89d07356217cb10a76caa128b540792d6a
jhoarau <jeanemmanuel.hoarau@etu.enseeiht.fr>
2021-12-10 16:22:52 +0100

    update

--- classification_with_knn.py
+++ classification_with_knn.py
@@ -4,8 +4,8 @@ from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
 
 
-#TODO change comment to english please
-# importation des données
+
+# importation data of the features and labels
 features_1 = np.load(
     "C:/Users/jemho/Documents/Etude/hambourg/cours_hambourg/intelligent system in medicin/pbl/Feature Vectors/X_train_gray_average.npy")
 features_2 = np.load(
@@ -22,6 +22,7 @@ features_6 = np.load(
 labels = np.load(
     "C:/Users/jemho/Documents/Etude/hambourg/cours_hambourg/intelligent system in medicin/pbl/Feature Vectors/y_train.npy")
 
+
 ver_1 = np.load(
     "C:/Users/jemho/Documents/Etude/hambourg/cours_hambourg/intelligent system in medicin/pbl/Feature Vectors/X_ver_gray_average.npy")
 ver_2 = np.load(
@@ -37,6 +38,25 @@ ver_6 = np.load(
 
 y_ver = np.load(
     "C:/Users/jemho/Documents/Etude/hambourg/cours_hambourg/intelligent system in medicin/pbl/Feature Vectors/y_ver.npy")
+
+
+# watch if the labels are uniformly samples
+
+# c1,c2,c3,c4 = 0,0,0,0
+# for x in range(0,len(labels)):
+#     if (labels[x] == 0.):
+#         c1+=1
+#     elif (labels[x] == 1.):
+#         c2+=1
+#     elif (labels[x] == 2.):
+#         c3+=1
+#     elif (labels[x] == 3.):
+#         c4+=1
+#
+# print("samples are ",c1,c2,c3,c4)
+# c1,c2,c3,c4 = c1/len(labels),c2/len(labels),c3/len(labels),c4/len(labels)
+# print("samples are ",c1,c2,c3,c4)
+
 
 
 # Normalization of the features
@@ -67,58 +87,44 @@ normalize(ver_6, lower_bound, upper_bound)
 
 # create features matrix to train the model
 
-features = np.array([features_1, features_2, features_3,
-                    features_4, features_5, features_6])
-# features = np.array([features_2,features_3,features_5,features_6])
+features = np.array([features_1, features_2, features_3,features_4, features_5, features_6])
+# features = np.array([features_1])
 features = np.transpose(features)
 
 # create features matrix to test the model
 
 ver = np.array([ver_1, ver_2, ver_3, ver_4, ver_5, ver_6])
-# ver = np.array([ver_2,ver_3,ver_5,ver_6])
+# ver = np.array([ver_1])
 ver = np.transpose(ver)
 
-#TODO change comment to english
-# création et entrainement du model
 
-# creating and training the model
+# creating and training the model for k neighbors
 
-# knn_model = KNeighborsClassifier(n_neighbors=2000,weights = 'distance')
+# knn_model = KNeighborsClassifier(n_neighbors=20, weights = 'distance')
 # knn_model.fit(features,labels)
 #
-# # prediction f the model
 # predicted = knn_model.predict(ver)
 # acc = accuracy_score(y_ver,predicted)
 # print(acc)
 
-# loop to search the best numbers of neighbour
-
-# ce que fait le accuracy_score
-# compteur = 0
-#
-# for k in range(1,len(ver_1)):
-#     if (predicted[k] == y_ver[k]):
-#         compteur += 1
-# print(compteur/3386)
-
-
-#TODO change comment to english
-# cherccher le meilleur nombre de voisin
+# loop to search the best numbers of neighbour between 1 and 500
 
 L1=[]
 L2=[]
-for k in range(1,500,5):
-    knn_model = KNeighborsClassifier(n_neighbors=k)
+for k in range(500,600,5):
+    knn_model = KNeighborsClassifier(n_neighbors=k, weights = 'distance')
     knn_model.fit(features, labels)
     predicted = knn_model.predict(ver)
     acc = accuracy_score(y_ver, predicted)
     L1.append(k)
     L2.append(acc)
+
+
 max_value = max(L2)
 max_index = L2.index(max_value)
 best_n_neighbour = L1[max_index]
 
-print(max_value)
-print("best_neighbour is ", best_n_neighbour)
+print("best prediction is", max_value)
+print("and it is for k =  ", best_n_neighbour)
 plt.plot(L1, L2)
 plt.show()
