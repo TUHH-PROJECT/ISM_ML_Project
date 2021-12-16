12d19e4d1012164d6210d0b0a7fdd18efe33b4b4
Giuliano Taccogna <g-taccogna@web.de>
2021-12-16 20:14:58 +0100

    Function to create the txt files we have to submit
    To use it, we have to decide, which models to use

---
+++ Fct_Classify.py
@@ -0,0 +1,140 @@
+import numpy as np
+import os
+import pickle
+import joblib
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import RobustScaler
+
+def classify (Model, Dataset):
+    if Dataset == 'Test':
+        with open("/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Testing/Test/test_names", "rb") as f:
+            names = pickle.load(f)
+        f.close()
+        if Model == :
+            classi = joblib.load("/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/ML_Models/")
+            features = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Testing/Features/')
+            train_data = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Feature Vectors')
+            norm = .fit(train_data)
+        if Model == :
+            classi = joblib.load("/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/ML_Models/")
+            features = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Testing/Features/')
+            train_data = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Feature Vectors')
+            norm = .fit(train_data)
+        if Model == :
+            classi = joblib.load("/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/ML_Models/")
+            features = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Testing/Features')
+            train_data = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Feature Vectors')
+            norm = .fit(train_data)
+        if Model == :
+            classi = joblib.load("/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/ML_Models/")
+            features = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Testing/Features')
+            train_data = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Feature Vectors')
+            norm = .fit(train_data)
+        if Model == :
+            classi = joblib.load("/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/ML_Models/")
+            features = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Testing/Features')
+            train_data = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Feature Vectors')
+            norm = .fit(train_data)
+    
+    if Dataset == 'Noisy_Test':
+        with open("/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Testing/Noisy_Test/test_noise_names", "rb") as f:
+            names = pickle.load(f)
+        f.close()
+        if Model == :
+            classi = joblib.load("/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/ML_Models/")
+            features = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Testing/Features/')
+            train_data = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Feature Vectors')
+            norm = .fit(train_data)
+        if Model == :
+            classi = joblib.load("/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/ML_Models/")
+            features = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Testing/Features/')
+            train_data = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Feature Vectors')
+            norm = .fit(train_data)
+        if Model == :
+            classi = joblib.load("/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/ML_Models/")
+            features = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Testing/Features/')
+            train_data = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Feature Vectors')
+            norm = .fit(train_data)
+        if Model == :
+            classi = joblib.load("/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/ML_Models/")
+            features = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Testing/Features/')
+            train_data = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Feature Vectors')
+            norm = .fit(train_data)
+        if Model == :
+            classi = joblib.load("/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/ML_Models/")
+            features = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Testing/Features/')
+            train_data = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Feature Vectors')
+            norm = .fit(train_data)
+
+    if Dataset == 'Mtec':
+        with open("/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Testing/Mtec_test/mtec_test_names", "rb") as f:
+            names = pickle.load(f)
+        f.close()
+        if Model == :
+            classi = joblib.load("/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/ML_Models/")
+            features = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Testing/Features/')
+            train_data = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Feature Vectors')
+            norm = .fit(train_data)
+        if Model == :
+            classi = joblib.load("/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/ML_Models/")
+            features = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Testing/Features/')
+            train_data = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Feature Vectors')
+            norm = .fit(train_data)
+        if Model == :
+            classi = joblib.load("/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/ML_Models/")
+            features = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Testing/Features/')
+            train_data = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Feature Vectors')
+            norm = .fit(train_data)
+        if Model == :
+            classi = joblib.load("/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/ML_Models/")
+            features = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Testing/Features/')
+            train_data = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Feature Vectors')
+            norm = .fit(train_data)
+        if Model == :
+            classi = joblib.load("/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/ML_Models/")
+            features = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Testing/Features/')
+            train_data = np.load('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Feature Vectors')
+            norm = .fit(train_data)
+    
+    #Normalization
+    features_norm = norm.transform(features)
+
+    #Prediction
+    y_pred = classi.predict(features_norm)
+
+    #Translating the integers in y_pred in the actual labels
+    classes = [None]*len(y_pred)
+    i = 0
+    for int in y_pred:
+        if int == 0:
+            classes[i] = 'Normal'
+        if int == 1:
+            classes[i] = 'COVID'
+        if int == 2:
+            classes[i] = 'pneumonia'
+        if int == 3:
+            classes[i] = 'Lung_Opacity'
+        i = i+1
+
+    #Writing the result in a text file
+    f = open(os.path.join('/Users/giulianotaccogna/Documents/Development/Python/ISM_ML_Project/Data/Testing/Classifications', Dataset+'_'+Model+'.txt'), 'w', encoding='utf-8')
+    for i in range(len(names)):
+        f.write(names[i]+' '+classes[i]+'\n')
+    f.close()
+
+
+Datasets = ['Test', 'Noisy_Test', 'Mtec']
+Models = []
+
+for dataset in Datasets:
+    for model in Models:
+        classify(model, dataset)
+    
+
+
+
+
+  
+   
+
+
